b321c3b3db951f9c6d3ddc7a2ad018d114368975
TymashArseny <aresenijtumas@gmail.com>
2024-12-08 17:54:23 +0300

    first commit

---
+++ practical_work.py
@@ -0,0 +1,58 @@
+# number = int(input("Введите введите любое число "))
+
+# if number > 0:
+#     print("число положительное")
+# elif number< 18:
+#     print ("число отрицацельное")
+# else:
+#     print("число 0")
+
+
+
+# number = int(input("Введите сколько вам лет "))
+
+
+# if number >= 18:
+#     print("Вы совершено летний")
+
+#     product = input("пиво или водка")
+#     if product == "пиво":
+#        print("с вас 2 рубля")
+#     elif product == "водка":
+#         print("с вас 5 рублей")
+
+
+
+# else:
+#      print ("вы не совершено летний")
+
+    
+
+# print("меню")
+# print("1. Сложение\n2. Отнимание\n3. Умножэние\n4. Деление\n5. Выйти из програмы")
+# choice = int(input(""))
+
+# a = int(input("Введите число a"))
+# b = int(input("Введите число b"))
+
+# if choice == 1:
+#     c = a + b
+#     print(c)
+# elif choice == 2:
+#     c = a - b
+#     print(c)
+# elif choice == 3:
+#     c = a * b
+#     print (c)
+# elif choice == 4:
+#     c = a / b
+#     print(c)
+
+
+number = int(input("Введите любое число"))
+
+if number >10 and number < 20:
+    print (" введёное число входит в деопозон от 10 до 20")
+
+
+
